655dea06f23d60a637a9867daa9283ada330121e
gogumario <ggmario@naver.com>
2017-10-18 15:25:26 +0900

    사용하지 않은 url 제거

--- app/src/main/java/com/eluo/signage/kotlin/utils/WebViewSetting.kt
+++ app/src/main/java/com/eluo/signage/kotlin/utils/WebViewSetting.kt
@@ -6,7 +6,6 @@ package com.eluo.signage.kotlin.utils
 
 //url 정보
 fun formatUrl(): String = "http://www.eluocnc.com/GW_V3/signage/default.asp?scd="   //기본 페이지 URL
-//fun formatUrl(): String = "http://www.eluocnc.com/GW_V3/signage/test1.asp?scd="   //기본 페이지 URL
 fun verIntroUrl():String ="http://www.eluocnc.com/GW_V3/app/signageList.asp?"       //인트로 체크사용 URL
 fun viewPdfUrl(): String = "https://docs.google.com/viewer?url="                    //구글 pdf 뷰어 URL
 fun googleDocslUrl(): String ="https://docs.google.com/viewer?url="                 //구글 독스 URL
